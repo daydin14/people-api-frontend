72a4b28e834cdb08139423fe12bd9299af9bc616
David Aydin <david.aydin6@gmail.com>
2022-06-13 16:07:26 -0400

    Sass file with content

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header';
 import Main from './components/Main';
 import Index from './pages/Index';
 import Show from './pages/Show';
-import "./styles.css";
+
 
 function App() {
   return (
