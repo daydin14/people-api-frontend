84f89114c213ea9f6b3c399dd523685f920acee0
David Aydin <david.aydin6@gmail.com>
2022-06-14 10:04:45 -0400

    Show Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,13 +27,13 @@ const Index = (props) => {
 
   const loaded = () => {
     return props.people.map((person) => (
-      <div key={person._id} className="person">
+      <li key={person._id} className="person">
         <Link to={`/people/${person._id}`}>
           <h1>{person.name}</h1>
         </Link>
         <img src={person.img} alt={person.name} />
         <h3>{person.title}</h3>
-      </div>
+      </li>
     ));
   };
 
@@ -67,7 +67,7 @@ const Index = (props) => {
         />
         <input type="submit" value="Create Person" />
       </form>
-      {props.people ? loaded() : loading()}
+      {props.people ? <ol>{loaded()}</ol> : loading()}
     </section>
   );
 };
--- src/pages/show.js
+++ src/pages/show.js
@@ -1,7 +1,21 @@
 const Show = (props) => {
+  const avatarUrl =
+    "https://pixabay.com/vectors/avatar-icon-placeholder-facebook-1577909/";
+  const id = props.match.params.id;
+  const person = props.people.find(function (p) {
+    return p._id === id;
+  }); // Or ".find(p => p._id === id);
+
   return (
     <div>
-      <h1>Show Component</h1>
+      <h1>{person.name}</h1>
+      <h2>{person.title}</h2>
+
+      {person.img ? (
+        <img src={person.img} alt={person.name} />
+      ) : (
+        <img src={avatarUrl} alt={"placeholder"} />
+      )}
     </div>
   );
 };
